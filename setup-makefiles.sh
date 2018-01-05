5514b2cb8fecb2b391e25d6032bc428f29c26dbb
razorloves <razorloves@gmail.com>
2018-01-05 08:26:36 -0600

    marlin: update proprietary files for sprint/vzw/fi
    Change-Id: I78329a03f8c1fa411e3eb5225dfc53507e7058a7

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
 # limitations under the License.
 
 set -e
+
+INITIAL_COPYRIGHT_YEAR=2017
 
 VENDOR=google
 DEVICE=marlin
