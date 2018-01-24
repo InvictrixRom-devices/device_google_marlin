948982b3e079d1812664d1571f4fba84f7e27547
Joey <joey@lineageos.org>
2018-01-24 11:24:37 +0100

    marlin: add textclassifier smart selection bundle
    This adds smart selection models for multiple
    languages, instead of just the default english.
    
    Change-Id: I50d8173d24c4cab773824107b98929dbbc469783
    Signed-off-by: Joey <joey@lineageos.org>

--- device-lineage.mk
+++ device-lineage.mk
@@ -15,3 +15,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
 # Pixel Experience
 PRODUCT_COPY_FILES += \
     device/google/marlin/nexus.xml:system/etc/sysconfig/nexus.xml
+
+# TextClassifier smart selection model files
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
