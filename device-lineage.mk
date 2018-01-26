27cf43d55428b618a45529fbfb674764bcb8c00f
razorloves <razorloves@gmail.com>
2018-01-26 09:19:04 -0600

    marlin: build apps needed for IMS
    Change-Id: I45d58b5fadc00191ffe1f8f334d8dc4265ecb4a2

--- device-lineage.mk
+++ device-lineage.mk
@@ -3,6 +3,12 @@ PRODUCT_PACKAGES += \
     libion \
     Snap
 
+# IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
 
