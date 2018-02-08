18bd5226f66fdd7673b9810411fdc02abe1f37c9
neporood <neporood@live.com>
2018-02-08 06:09:10 -0600

    Update Fingerprint: OPM1.171019.016
    Change-Id: I898cfc132ba12c8678bec3e31f0d74a9d2cb0f31

--- fh.mk
+++ fh.mk
@@ -10,7 +10,7 @@ $(call inherit-product, device/google/marlin/aosp_marlin.mk)
 
 -include device/google/marlin/marlin/device-lineage.mk
 
-## Device identifier. This must come after all inclusions
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := fh_marlin
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel XL
@@ -19,7 +19,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=marlin \
-    BUILD_FINGERPRINT=google/marlin/marlin:8.1.0/OPM1.171019.012/4470837:user/release-keys \
-    PRIVATE_BUILD_DESC="marlin-user 8.1.0 OPM1.171019.012 4470837 release-keys"
+    BUILD_FINGERPRINT=google/marlin/marlin:8.1.0/OPM1.171019.016/4503492:user/release-keys \
+    PRIVATE_BUILD_DESC="marlin-user 8.1.0 OPM1.171019.016 4503492 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
+
