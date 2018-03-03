f6f36a7513c6050da273e07b8bbbb56a3cb5e5a2
neporood <neporood@gmail.com>
2018-03-03 10:52:13 -0600

    Revert "marlin: Add Google platform certificate"
    This reverts commit 100a58d742f6dec00fd08962b822da37966893f0.

--- marlin/device-lineage.mk
+++ marlin/device-lineage.mk
@@ -1,4 +1,1 @@
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/marlin/marlin/overlay-lineage
-
 $(call inherit-product, device/google/marlin/device-lineage.mk)
--- sailfish/device-lineage.mk
+++ sailfish/device-lineage.mk
@@ -1,4 +1,1 @@
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/marlin/sailfish/overlay-lineage
-
 $(call inherit-product, device/google/marlin/device-lineage.mk)
