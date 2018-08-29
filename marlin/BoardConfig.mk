ea85163df6f8b1cebd951403d3fab01e5e3322ad
DeviousFusion <dj3tusk@gmail.com>
2018-08-29 04:46:58 -0500

    kernel: unicornblood

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -128,7 +128,7 @@ endif
 # Kernel
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_COMPILE_WITH_MSM_KERNEL := true
-TARGET_KERNEL_CONFIG := marlin_defconfig #TODO: defconfig
+TARGET_KERNEL_CONFIG := du_marlin_defconfig
 TARGET_KERNEL_SOURCE := kernel/google/marlin
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
